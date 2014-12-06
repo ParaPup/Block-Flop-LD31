81e8da9ca97b1a84f3019063d8db410d26f02ac0
HunterTheHusky <aazahunter114@gmail.com>
2014-12-06 16:53:32 +1100

    Player Spawn/ Basic Movement
    Unfinished movement and test platform.

---
+++ Game/Assets/Scripts/bfPlayerSpawnPosition.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bfPlayerSpawnPosition : MonoBehaviour {
+	public float PosX = 1f;
+	public float PosY = 1f;
+	public float PosZ = 1f;
+	// Use this for initialization
+	void Start () {
+		transform.position = new Vector3 (PosX, PosY, PosZ);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
